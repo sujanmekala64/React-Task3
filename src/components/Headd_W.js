610faf31417c562afe94f37792e43c17c97d2a80
Sujan <sujanmekala64@gmail.com>
2023-10-12 19:36:45 +0530

    first commit

--- src/components/Headd_W.js
+++ src/components/Headd_W.js
@@ -1,11 +1,11 @@
  import "../../src/App.css";
  function Headd(){
     const myStyle = {
-        color: "pink", backgroundColor: "red"
+        color: "black", backgroundColor: "red"
     }
     return(
         <div class="bg-dark text-center text-primary">  {/**After importing bootstrap in App.js we shhould give classes of BS */}
-            <h1 style={{color:"red"}}>Shoppy Website</h1>   {/**Inline styling */}
+            <h1 style={{color:"white"}}>Shoppy Website</h1>   {/**Inline styling */}
             <h2 style={myStyle}>Low cost more products</h2> {/**Internal styling */}
             <p id="i1">Explore more various products</p>    {/**External styling  here that i1 is not working check once*/}
         </div>
