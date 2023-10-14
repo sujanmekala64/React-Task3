c56e497a218e29e4ec81991ef8f8069f45b68130
Sujan <sujanmekala64@gmail.com>
2023-10-14 18:19:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-// import logo from './logo.svg';
+import React from 'react';
+import ColorPicker from './ColorPicker';
 import './App.css';
-import Header from './components/Header';
-import Education from './components/Education';
-import Skills from './components/Skills';
-import Footer from './components/Footer';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
+  const colors = ['#fe0000', '#00ff01', '#0000fe','#ffff00', '#ff00fe', '#00ffff', '#fea500','#81007f','#ffc0cb','#008001','#fe6347','#01ced1','#8b4512','#ff8b00','#4682b4','#fed700'];
+
   return (
-   <div>
-    <Header />
-    <Education />
-    <Skills/>
-    <Footer/>
-   </div>
+    <div className="App">
+      <h1 className='mt-4'><b>Color Picker</b></h1>
+      <ColorPicker colors={colors} />
+    </div>
   );
 }
-
 export default App;
